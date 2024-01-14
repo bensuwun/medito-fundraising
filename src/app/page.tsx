c5908330e3fc4d6e98c665eeb0af0c19a187ba39
jtp <benedict_sun@dlsu.edu.ph>
2024-01-14 14:19:50 +0800

    Initial changes before mantine integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Navbar from "../components/Navbar";
+import Progress from "../components/Progress";
+import project from "../../public/project.json";
+
 export default function Home() {
+  const proj = project.data.fundraisingProject;
   return (
-    <p>Hello World</p>
+    <>
+      <Navbar />
+
+      <div className="flex flex-col gap-3 p-7">
+        <h1 className="font-bold text-2xl">{proj.name}</h1>
+        <p>{proj.description}</p>
+        {/* Progress Bar */}
+        <Progress />
+
+        {/* Donate Button */}
+        <div>
+
+        </div>
+      </div>
+      
+    </>
   )
 }
---
+++ src/components/Progress.tsx
@@ -0,0 +1,23 @@
+/**
+ * Props:
+ *  - Amount Raised
+ *  - Target Amount
+ */
+export default function ProgressBar() {
+    return (
+        <div className="flex flex-col items-center">
+            <p>
+                <span className="font-bold">US$ 7,000</span> raised
+            </p>
+            {/* Progress Bar Container */}
+            <div className="flex w-full rounded-md relative h-3 bg-gray-200">
+                <div className="rounded-md border bg-gray-400 relative w-1/2">
+
+                </div>
+            </div>
+            <p>
+                of goal <span className="font-bold">US$ 10,000 </span>
+            </p>
+        </div>
+    )
+};
