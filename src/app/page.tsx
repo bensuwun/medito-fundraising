8deaa422375c7a96d7ba7dacb838e8c1f1d16170
jtp <benedict_sun@dlsu.edu.ph>
2024-01-18 17:12:50 +0800

    Moved fetch deeper in tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,21 @@ import ProjectDetails from "@/components/project/ProjectDetails";
 import ProjectTabs from "@/components/project/ProjectTabs/ProjectTabs";
 import DonationNotif from "@/components/project/DonationNotif/DonationNotif";
 import CURRENCY from "@/helpers/currencies";
-import { getTotalAmountRaised } from "@/helpers/requests";
+
 
 export default async function Home() {
   // Obtain project details
   const proj = project.data.fundraisingProject;
-
-  // Fetch donation data - total amount raised
-  var totalAmtRaised: number = await getTotalAmountRaised();
-
 
   return (
     <>
       {/* Client */}
       <DonationNotif />
 
-      {/* Server */}
       <ProjectDetails
         name={proj.name}
         description={proj.description}
         targetAmt={proj.targetAmt}
-        totalAmtRaised={totalAmtRaised}
       />
 
       {/* Client - Tabs and Panels */}
--- src/components/project/ProjectDetails.tsx
+++ src/components/project/ProjectDetails.tsx
@@ -1,23 +1,24 @@
 import Progress from "./Progress";
 import Donate from "./Donate/Donate";
 import { Card } from "@mantine/core";
+import { getTotalAmountRaised } from "@/helpers/requests";
 
 /**
  * Displays the title, description, and the current progress and target goal of the fundraising initiative.
  */
-export default function ProjectDetails (
+export default async function ProjectDetails (
     {
         name,
         description,
         targetAmt,
-        totalAmtRaised
     } : {
         name: string,
         description: string,
         targetAmt: number,
-        totalAmtRaised: number
     }) 
 {
+    const totalAmtRaised = await getTotalAmountRaised();
+
     return (
         <div className="flex flex-col gap-3 px-7 py-page-sm lg:py-page container mx-auto lg:grid lg:grid-cols-8">
             {/* Project Description */}
@@ -27,7 +28,6 @@ export default function ProjectDetails (
             </div>
             
             {/* Progress and Donate Container */}
-            
             <Card shadow="sm" radius="md" withBorder className="px-5 py-5 gap-8 col-span-3">
                 {/* Progress Bar */}
                 <Progress targetAmt={targetAmt} totalAmtRaised={totalAmtRaised}/>
